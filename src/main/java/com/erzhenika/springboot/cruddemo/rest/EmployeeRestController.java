223b1a46baf648679c3bb514eef9f3c4ef81a9d3
erzhena <erzhenadulzynova@gmail.com>
2025-12-29 15:25:27 +0000

    Add DELETE call to RestController

--- src/main/java/com/erzhenika/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/erzhenika/springboot/cruddemo/rest/EmployeeRestController.java
@@ -80,4 +80,17 @@ public class EmployeeRestController {
         return dbEmployee;
     }
 
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+
+        Employee employee = employeeService.findById(employeeId);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee id - " + employeeId;
+    }
 }
