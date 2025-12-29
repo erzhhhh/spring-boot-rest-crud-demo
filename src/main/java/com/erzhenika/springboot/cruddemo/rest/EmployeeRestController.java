58f01e8a87ff2c6af1efd77d79c4c3b38cb560b9
erzhena <erzhenadulzynova@gmail.com>
2025-12-29 11:01:45 +0000

    Add GET call to RestController

--- src/main/java/com/erzhenika/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/erzhenika/springboot/cruddemo/rest/EmployeeRestController.java
@@ -3,9 +3,7 @@ package com.erzhenika.springboot.cruddemo.rest;
 import com.erzhenika.springboot.cruddemo.entity.Employee;
 import com.erzhenika.springboot.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +21,18 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+
+        Employee employee = employeeService.findById(employeeId);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found " + employeeId);
+        } else {
+            return employee;
+        }
     }
 
 }
