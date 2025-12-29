14ff030c2beab5e2b3daa731b5b2c7d70c72f9ef
erzhena <erzhenadulzynova@gmail.com>
2025-12-29 10:38:21 +0000

    Add service methods
    Delegating the calls to the DAO

--- src/main/java/com/erzhenika/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/erzhenika/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.erzhenika.springboot.cruddemo.service;
 
 import com.erzhenika.springboot.cruddemo.dao.EmployeeDAO;
 import com.erzhenika.springboot.cruddemo.entity.Employee;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,5 +21,22 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        return employeeDAO.findById(id);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(Employee employee) {
+        return employeeDAO.save(employee);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(int id) {
+        employeeDAO.deleteById(id);
     }
 }
