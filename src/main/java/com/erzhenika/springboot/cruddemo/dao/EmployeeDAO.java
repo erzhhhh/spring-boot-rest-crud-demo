25e69e0faf21bf18f2d87fada476d6f3c6aabef9
Erzhena <troublemakergirl212@gmail.com>
2025-12-30 07:24:44 +0000

    Revert "Add JPA Repository"

---
+++ src/main/java/com/erzhenika/springboot/cruddemo/dao/EmployeeDAO.java
@@ -0,0 +1,17 @@
+package com.erzhenika.springboot.cruddemo.dao;
+
+import com.erzhenika.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    List<Employee> findAll();
+
+    Employee findById(int id);
+
+    Employee save(Employee employee);
+
+    void deleteById(int id);
+
+}
--- src/main/java/com/erzhenika/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/erzhenika/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -1,52 +1,42 @@
 package com.erzhenika.springboot.cruddemo.service;
 
-import com.erzhenika.springboot.cruddemo.dao.EmployeeRepository;
+import com.erzhenika.springboot.cruddemo.dao.EmployeeDAO;
 import com.erzhenika.springboot.cruddemo.entity.Employee;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
-    private EmployeeRepository employeeRepository;
+    private EmployeeDAO employeeDAO;
 
     @Autowired
-    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
-        this.employeeRepository = employeeRepository;
+    public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
     }
 
     @Override
     public List<Employee> findAll() {
-        return employeeRepository.findAll();
+        return employeeDAO.findAll();
     }
 
     @Override
     public Employee findById(int id) {
-
-        // Optional was introduced in Java 8 for handling NullPointerException
-        Optional<Employee> result = employeeRepository.findById(id);
-
-        Employee employee = null;
-
-        if (result.isPresent()) {
-            employee = result.get();
-        } else {
-            throw new RuntimeException("Did not find employee id - " + id);
-        }
-
-        return employee;
+        return employeeDAO.findById(id);
     }
 
+    @Transactional
     @Override
     public Employee save(Employee employee) {
-        return employeeRepository.save(employee);
+        return employeeDAO.save(employee);
     }
 
+    @Transactional
     @Override
     public void deleteById(int id) {
-        employeeRepository.deleteById(id);
+        employeeDAO.deleteById(id);
     }
 }
